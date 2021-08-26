9099beb85b7580dba80b08216a34a39e15ce91c0
Miles Gould <miles@assyrian.org.uk>
2021-08-26 22:26:30 +0100

    Add DS solution for reusable barrier problem

---
+++ chapter3/reusable_barrier.py
@@ -0,0 +1,30 @@
+n = 4
+count = 0
+mutex = Semaphore(1)
+turnstile1 = Semaphore(0)
+turnstile2 = Semaphore(1)
+laps = {'A-' + str(i): 0 for i in range(n)}
+
+## Thread A * 4
+# Rendezvous
+laps[pid()] += 1
+mutex.wait()
+    count = count + 1
+    if count == n: turnstile1.signal()
+    if count == n: turnstile2.wait()
+mutex.signal()
+turnstile1.wait()
+turnstile1.signal()
+
+# Critical point
+assert max(laps.values()) == min(laps.values()), laps
+
+mutex.wait()
+    count -= 1
+    if count == 0: turnstile1.wait()
+    if count == 0: turnstile2.signal()
+mutex.signal()
+turnstile2.wait()
+turnstile2.signal()
+
+balk()
