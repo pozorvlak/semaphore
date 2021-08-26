18b4fb45dfdb36d6e39bb4a37ff25303368a6fb7
Miles Gould <miles@assyrian.org.uk>
2021-08-26 15:44:13 +0100

    Working barrier solution

--- chapter3/barrier.py
+++ chapter3/barrier.py
@@ -1,47 +1,78 @@
-finished = Semaphore(-5)
+n = 4
+count = 0
+mutex = Semaphore(1)
+barrier = Semaphore(0)
+finished_mutex = Semaphore(1)
+finished_count = 0
 thread_finished = Semaphore(0)
-arrived = Semaphore(-4)
-all_arrived = Semaphore(0)
+all_finished = Semaphore(0)
 
 ## Thread 1
-arrived.signal()
-all_arrived.wait()
+print("Thread 1 at rendezvous")
+mutex.wait()
+count = count + 1
+mutex.signal()
+if count < n :
+    barrier.wait()
+else:
+    barrier.signal(count)
 print("Thread 1 finished!")
-finished.signal()
-thread_finished.wait()
+finished_mutex.wait()
+finished_count += 1
+finished_mutex.signal()
+thread_finished.signal()
+all_finished.wait()
 
 ## Thread 2
-arrived.signal()
-all_arrived.wait()
+print("Thread 2 at rendezvous")
+mutex.wait()
+count = count + 1
+mutex.signal()
+if count < n:
+    barrier.wait()
+else:
+    barrier.signal(count)
 print("Thread 2 finished!")
-finished.signal()
-thread_finished.wait()
+finished_mutex.wait()
+finished_count += 1
+finished_mutex.signal()
+thread_finished.signal()
+all_finished.wait()
 
 ## Thread 3
-arrived.signal()
-all_arrived.wait()
+print("Thread 3 at rendezvous")
+mutex.wait()
+count = count + 1
+mutex.signal()
+if count < n:
+    barrier.wait()
+else:
+    barrier.signal(count)
 print("Thread 3 finished!")
-finished.signal()
-thread_finished.wait()
+finished_mutex.wait()
+finished_count += 1
+finished_mutex.signal()
+thread_finished.signal()
+all_finished.wait()
 
 ## Thread 4
-arrived.signal()
-all_arrived.wait()
+print("Thread 4 at rendezvous")
+mutex.wait()
+count = count + 1
+mutex.signal()
+if count < n:
+    barrier.wait()
+else:
+    barrier.signal(count)
 print("Thread 4 finished!")
-finished.signal()
-thread_finished.wait()
-
-## Thread barrier
-arrived.wait()
-all_arrived.signal()
-all_arrived.signal()
-all_arrived.signal()
-all_arrived.signal()
-print("Barrier reached!")
-finished.signal()
-thread_finished.wait()
+finished_mutex.wait()
+finished_count += 1
+finished_mutex.signal()
+thread_finished.signal()
+all_finished.wait()
 
 ## Thread killer
-finished.wait()
-print("All threads have finished!")
-exit()
+thread_finished.wait()
+if finished_count == n:
+    print("All threads have finished!")
+    exit()
