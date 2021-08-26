0140e3a88a1c8898bd519fc310ec62542c126d51
Miles Gould <miles@assyrian.org.uk>
2021-08-26 13:04:59 +0100

    Add solution to Rendezvous problem

---
+++ chapter3/rendezvous.py
@@ -0,0 +1,30 @@
+# We want to guarantee that a1 happens before b2 and b1 happens before a2
+a1 = False
+b1 = False
+a1_sem = Semaphore(0)
+b1_sem = Semaphore(0)
+finished = Semaphore(-2)
+thread_finished = Semaphore(0)
+
+
+## Thread A
+a1 = False
+a1 = True # statement a1
+a1_sem.signal()
+b1_sem.wait()
+assert b1 # statement a2
+finished.signal()
+thread_finished.wait()
+
+## Thread B
+b1 = False
+b1 = True # statement b1
+b1_sem.signal()
+a1_sem.wait()
+assert a1 # statement b2
+finished.signal()
+thread_finished.wait()
+
+## Thread killer
+finished.wait()
+exit()
