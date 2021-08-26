d6f0906f0b14a243d92ceae3df5ef829037db20e
Miles Gould <miles@assyrian.org.uk>
2021-08-26 17:49:56 +0100

    Remove homegrown finished check from barrier

--- chapter3/barrier.py
+++ chapter3/barrier.py
@@ -2,10 +2,6 @@ n = 4
 count = 0
 mutex = Semaphore(1)
 barrier = Semaphore(0)
-finished_mutex = Semaphore(1)
-finished_count = 0
-thread_finished = Semaphore(0)
-all_finished = Semaphore(0)
 
 ## Thread 1
 print("Thread 1 at rendezvous")
@@ -17,11 +13,6 @@ if count < n :
 else:
     barrier.signal(count)
 print("Thread 1 finished!")
-finished_mutex.wait()
-finished_count += 1
-finished_mutex.signal()
-thread_finished.signal()
-all_finished.wait()
 
 ## Thread 2
 print("Thread 2 at rendezvous")
@@ -33,11 +24,6 @@ if count < n:
 else:
     barrier.signal(count)
 print("Thread 2 finished!")
-finished_mutex.wait()
-finished_count += 1
-finished_mutex.signal()
-thread_finished.signal()
-all_finished.wait()
 
 ## Thread 3
 print("Thread 3 at rendezvous")
@@ -49,11 +35,6 @@ if count < n:
 else:
     barrier.signal(count)
 print("Thread 3 finished!")
-finished_mutex.wait()
-finished_count += 1
-finished_mutex.signal()
-thread_finished.signal()
-all_finished.wait()
 
 ## Thread 4
 print("Thread 4 at rendezvous")
@@ -65,15 +46,3 @@ if count < n:
 else:
     barrier.signal(count)
 print("Thread 4 finished!")
-finished_mutex.wait()
-finished_count += 1
-finished_mutex.signal()
-thread_finished.signal()
-all_finished.wait()
-
-## Thread killer
-thread_finished.wait()
-if finished_count == n:
-    print("All threads have finished!")
-    exit()
-balk()
