5875df9c5dee9239f52fbfb9c833e7d2263814f8
Miles Gould <miles@assyrian.org.uk>
2021-08-26 12:44:47 +0100

    Use local temp variables in concurrent_updates

--- concurrent_updates.py
+++ concurrent_updates.py
@@ -1,13 +1,13 @@
-temp, count = 0, 0
+a_temp, b_temp, count = 0, 0, 0
 a_count, b_count = 0, 0
 
 ## Thread A
-temp = count
-count = temp + 1
+a_temp = count
+count = a_temp + 1
 a_count += 1
 print(count, a_count + b_count)
 
 ## Thread B
-temp = count
-count = temp + 1
+b_temp = count
+count = b_temp + 1
 b_count += 1
