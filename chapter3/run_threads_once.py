2357296854aec02cf89bb31856e6905a40e79042
Miles Gould <miles@assyrian.org.uk>
2021-08-26 15:24:37 +0100

    Actually correct run_threads_once solution!
    Turns out I'd completely misunderstood the meaning of
    Semaphore.signal(). Oh well.

--- chapter3/run_threads_once.py
+++ chapter3/run_threads_once.py
@@ -1,28 +1,43 @@
-finished = Semaphore(-4)
+thread_count = 4
+mutex = Semaphore(1)
+finished = 0
 thread_finished = Semaphore(0)
+all_finished = Semaphore(0)
 
 ## Thread A
 print("Thread A finished")
-finished.signal()
-thread_finished.wait()
+mutex.wait()
+finished += 1
+mutex.signal()
+thread_finished.signal()
+all_finished.wait()
 
 ## Thread B
-print("Thread A finished")
-finished.signal()
-thread_finished.wait()
+print("Thread B finished")
+mutex.wait()
+finished += 1
+mutex.signal()
+thread_finished.signal()
+all_finished.wait()
 
 ## Thread C
-print("Thread A finished")
-finished.signal()
-thread_finished.wait()
+print("Thread C finished")
+mutex.wait()
+finished += 1
+mutex.signal()
+thread_finished.signal()
+all_finished.wait()
 
 ## Thread D
-print("Thread A finished")
-finished.signal()
-thread_finished.wait()
+print("Thread D finished")
+mutex.wait()
+finished += 1
+mutex.signal()
+thread_finished.signal()
+all_finished.wait()
 
 ## Thread killer
-finished.wait()
-print("All threads have finished!")
-exit()
 thread_finished.wait()
+if finished == thread_count:
+    print("All threads have finished!")
+    exit()
