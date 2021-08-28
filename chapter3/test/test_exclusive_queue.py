749adaac525c80abdbb16ee58bc74f812d4df0ec
Miles Gould <miles@assyrian.org.uk>
2021-08-28 19:21:25 +0100

    Only show trace of failing run

--- chapter3/test/test_exclusive_queue.py
+++ chapter3/test/test_exclusive_queue.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 
-from hypothesis import given, strategies as st
+from hypothesis import given, settings, strategies as st, HealthCheck
 
 import sync
 
 
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
 @given(st.random_module())
-def test_exclusive_queue(random):
+def test_exclusive_queue(capsys, random):
+    capsys.readouterr()
     program = Path(__file__).parent.parent / "exclusive_queue.py"
     sim = sync.Sync(sync.Options(delay=0, max_steps=1000), program)
     sim.run()
