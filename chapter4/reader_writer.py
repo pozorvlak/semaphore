665eb6ad8beef539c90fc6fabeac32916d26317d
Miles Gould <miles@assyrian.org.uk>
2021-08-30 17:02:01 +0100

    Add solution to reader-writer problem

--- chapter4/reader_writer.py
+++ chapter4/reader_writer.py
@@ -1,3 +1,4 @@
+# scenario setup and assertions
 readers = []
 writers = []
 
@@ -19,14 +20,30 @@ def write():
 def finish_writing():
     writers.remove(pid())
 
+# synchronisation variables
+mutex = Semaphore(1)
+readers_mutex = Semaphore(1)
+num_readers = 0
 
 ## Thread reader * 2
+readers_mutex.wait()
+    if num_readers == 0:
+        mutex.wait()
+    num_readers += 1
+readers_mutex.signal()
 read()
 finish_reading()
+readers_mutex.wait()
+    num_readers -= 1
+    if num_readers == 0:
+        mutex.signal()
+readers_mutex.signal()
 balk()
 
 
 ## Thread writer * 2
+mutex.wait()
 write()
 finish_writing()
+mutex.signal()
 balk()
